a593b5cac47c0f1b801c2bd04a1a528f9ad888f0
venkatesh <venkateshvenky626@gmail.com>
2025-05-24 16:06:10 +0530

    first commit

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -89,7 +89,7 @@ def call(Map configMap){
             stage('Deploy') {
                 when {
                     expression{
-                        params.Deploy
+                        params.Deploy=true
                     }
                 }
                 steps {
